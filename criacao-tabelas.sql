3a77cd22cffa2a54e907ebad0a21f00b6f888ef1
GiseleBCSantos <125358138+GiseleBCSantos@users.noreply.github.com>
2025-06-11 18:06:46 -0300

    Add scripts for creating and validating data

---
+++ criacao-tabelas.sql
@@ -0,0 +1,162 @@
+-- Metodo de pagamento
+create table  metodo_pagamento
+(
+  id_met_pag serial primary KEY,
+  nome varchar(50) not null,
+  descricao varchar(200) not null,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por varchar(50) null,
+  atualizado_em TIMESTAMP NULL
+ );
+ 
+ -- Plano
+ create table plano
+ (
+  id_pla serial primary key,
+  nome varchar(50) not null,
+  descricao varchar(200) not null,
+  duracao_meses int not null,
+  valor NUMERIC(10,2) not null,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   atualizado_por varchar(50) null,
+  atualizado_em TIMESTAMP NULL
+ );
+ 
+ -- Aluno
+ create table aluno
+ (
+   id_aluno serial primary key,
+   nome varchar(100) not null,
+   telefone varchar(50),
+   email varchar(50),
+   dt_nasc date not null,
+   genero CHAR NOT NULL CHECK (genero IN ('M', 'F', 'O'))
+   dt_cadastro date not null,
+   endereco varchar(100),
+   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   atualizado_por varchar(50) null,
+   atualizado_em TIMESTAMP NULL
+ );
+ 
+ -- funcionarios
+ create table funcionario
+ (
+   id_func serial primary key,
+   nome varchar(100) not null,
+   telefone varchar(50),
+   email varchar(50),
+   endereco varchar(100), 
+   dt_nasc date not null,
+   dt_admissao date not null,
+   turno varchar(20) not null,
+   carga_horaria int not null,
+   salario NUMERIC(10,2) not null,
+   tipo char not null,
+   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   atualizado_por varchar(50) null,
+   atualizado_em TIMESTAMP NULL
+ );
+ 
+ create table instrutor
+ (
+   qntd_alunos int,
+   especialidade varchar(20)
+ ) INHERITS  (funcionario);
+ 
+ create table atendente
+ (
+  permissao_caixa char
+ )
+ INHERITS  (funcionario);
+ 
+ -- equipamento
+ create table equipamento
+ (
+   id_equip serial primary key,
+   nome varchar(50) not null,
+   descricao varchar(200) not null,
+   data_aq date not null,
+   idade_min int not null,
+   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   atualizado_por varchar(50) null,
+   atualizado_em TIMESTAMP NULL
+ );
+ 
+ -- treino
+ create table treino
+ (
+   id_trei serial primary key,
+   descricao varchar(200) not null,
+   objetivo varchar(50),
+   duracao_meses int,
+   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   atualizado_por varchar(50) null,
+   atualizado_em TIMESTAMP NULL
+ ); 
+ 
+ -- item treino
+ create table item_treino 
+ (
+  id_item_treino serial primary key,
+  series int not null,
+  repeticoes int not null,
+  carga int,
+  id_trei int not null,
+  id_equip int not null,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+   atualizado_por varchar(50) null,
+  atualizado_em TIMESTAMP NULL,
+  foreign key (id_trei) references treino(id_trei),
+  foreign key (id_equip) references equipamento(id_equip)
+ );
+ 
+-- treino aluno
+create table treino_aluno
+(
+  id_trei_alu serial primary key,
+  dt_inicio date not null,
+  observacao varchar(200),
+  id_func int not null,
+  id_aluno int not null,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por varchar(50) null,
+  atualizado_em TIMESTAMP NULL,
+  foreign key (id_func) references funcionario (id_func),
+  foreign key (id_aluno) references aluno (id_aluno)
+);
+
+
+
+
+
+– matricula
+CREATE TABLE matricula (
+  id_matricula SERIAL PRIMARY KEY,
+  id_aluno INT NOT NULL,
+  id_plano INT NOT NULL,
+  id_met_pag INT NOT NULL,
+  id_atendente INT NOT NULL,
+  status VARCHAR(20) CHECK (status IN ('ativo', 'inativo', 'cancelado')),
+  valor_pag NUMERIC(10,2),
+  dt_matricula DATE NOT NULL,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP,
+
+  FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno),
+  FOREIGN KEY (id_plano) REFERENCES plano(id_pla),
+  FOREIGN KEY (id_met_pag) REFERENCES metodo_pagamento(id_met_pag),
+  FOREIGN KEY (id_atendente) REFERENCES funcionario(id_func)
+);
+
+
+
---
+++ validacao-tabelas.sql
@@ -0,0 +1,199 @@
+-- triggers para validação de dados
+ 
+ create or replace function func_valida_metodo_pag()
+ returns trigger as $$
+ begin 
+ 	if trim(new.nome) = '' THEN
+    	raise exception 'O campo "nome" não pode estar vazio';
+    end if;
+    if trim(new.descricao) = '' THEN
+    	raise EXCEPTION 'O campo "descrição" não pode estar vazio';
+    end if;
+    if (tg_op = 'update') THEN
+    	new.atualizado_em := CURRENT_TIMESTAMP;
+        new.atualizado_por := CURRENT_USER;
+    end if;
+
+    return new;
+
+ end ;
+ $$ language plpgsql; 
+ 
+ 
+ create or replace trigger valida_metodo_pag
+ before insert or UPDATE on metodo_pagamento
+ for each ROW
+ execute procedure func_valida_metodo_pag();
+ 
+ 
+
+
+ -- trigger para validação de plano
+ 
+ create or replace function func_valida_plano()
+ returns trigger as $$
+ begin 
+ 	if trim(new.nome) = '' THEN
+    	raise exception 'O campo "nome" não pode estar vazio';
+    end if;
+    if trim(new.descricao) = '' THEN
+    	raise EXCEPTION 'O campo "descrição" não pode estar vazio';
+    end if;
+    if new.duracao_meses is NULL THEN
+    	raise EXCEPTION 'O campo "duracao_meses" não pode estar vazio';
+    end if;
+    if new.valor is NULL THEN
+    	raise EXCEPTION 'O campo "valor" não pode estar vazio';
+    end if;
+    if (tg_op = 'update') THEN
+    	new.atualizado_em := CURRENT_TIMESTAMP;
+        new.atualizado_por := CURRENT_USER;
+    end if;
+
+    return new;
+
+ end ;
+ $$ language plpgsql; 
+ 
+ 
+ create or replace trigger valida_plano
+ before insert or UPDATE on plano
+ for each ROW
+ execute procedure func_valida_plano();
+ 
+
+-- trigger para validacao de aluno 
+ 
+ create or replace function func_valida_aluno()
+ returns trigger as $$
+ begin 
+ 	if trim(new.nome) = '' THEN
+    	raise exception 'O campo "nome" não pode estar vazio';
+    end if;
+    if new.dt_nasc is NULL THEN
+    	raise EXCEPTION 'O campo "data de nascimento" não pode estar vazio';
+    end if;
+    if new.genero is NULL THEN
+    	raise EXCEPTION 'O campo "genero" não pode estar vazio';
+    end if;
+    if (tg_op = 'update') THEN
+    	new.atualizado_em := CURRENT_TIMESTAMP;
+        new.atualizado_por := CURRENT_USER;
+    end if;
+    if (tg_op = 'insert') THEN
+    	new.dt_cadastro := CURRENT_TIMESTAMP;
+    end if;
+
+    return new;
+
+ end ;
+ $$ language plpgsql; 
+ 
+ create or replace trigger valida_aluno
+ before insert or UPDATE on aluno
+ for each ROW
+ execute procedure func_valida_aluno();
+ 
+ 
+ -- trigger para validacao de funcionario
+  
+ create or replace function func_valida_funcionario()
+ returns trigger as $$
+ begin 
+ 	if trim(new.nome) = '' THEN
+    	raise exception 'O campo "nome" não pode estar vazio';
+    end if;
+    if new.dt_nasc is NULL THEN
+    	raise EXCEPTION 'O campo "data de nascimento" não pode estar vazio';
+    end if;
+    if new.dt_admissao is NULL THEN
+    	raise EXCEPTION 'O campo "data de admissao" não pode estar vazio';
+    end if;
+    if trim(new.turno) = '' THEN
+    	raise exception 'O campo "turno" não pode estar vazio';
+    end if;
+    if trim(new.carga_horaria) is NULL THEN
+    	raise exception 'O campo "carga horaria" não pode estar vazio';
+    end if;
+    if trim(new.salario) is NULL THEN
+    	raise exception 'O campo "salario" não pode estar vazio';
+    end if;
+    if new.genero is NULL THEN
+    	raise EXCEPTION 'O campo "genero" não pode estar vazio';
+    end if;
+    if (tg_op = 'update') THEN
+    	new.atualizado_em := CURRENT_TIMESTAMP;
+        new.atualizado_por := CURRENT_USER;
+    end if;
+    return new;
+
+ end ;
+ $$ language plpgsql; 
+ 
+ create or replace trigger valida_funcionario
+ before insert or UPDATE on funcionario
+ for each ROW
+ execute procedure func_valida_funcionario();
+ 
+ 
+  -- trigger para validacao de equipamento
+  
+ create or replace function func_valida_equipamento()
+ returns trigger as $$
+ begin 
+ 	if trim(new.nome) = '' THEN
+    	raise exception 'O campo "nome" não pode estar vazio';
+    end if;
+    if trim(new.descricao) = '' THEN
+    	raise EXCEPTION 'O campo "descricao" não pode estar vazio';
+    end if;
+    if new.dt_aq is NULL THEN
+    	raise EXCEPTION 'O campo "data de aquisicao" não pode estar vazio';
+    end if;
+    if new.idade_min is NULL THEN
+    	raise exception 'O campo "idade minima" não pode estar vazio';
+    end if;
+    if (tg_op = 'update') THEN
+    	new.atualizado_em := CURRENT_TIMESTAMP;
+        new.atualizado_por := CURRENT_USER;
+    end if;
+    return new;
+
+ end ;
+ $$ language plpgsql; 
+ 
+ create or replace trigger valida_equipamento
+ before insert or UPDATE on equipamento
+ for each ROW
+ execute procedure func_valida_equipamento();
+ 
+ 
+   -- trigger para validacao de treino
+   
+ create or replace function func_valida_treino()
+ returns trigger as $$
+ begin 
+ 	if trim(new.descricao) = '' THEN
+    	raise exception 'O campo "descricao" não pode estar vazio';
+    end if;
+    if trim(new.objetivo) = '' THEN
+    	raise EXCEPTION 'O campo "objetivo" não pode estar vazio';
+    end if;
+    if new.duracao_meses is NULL THEN
+    	raise EXCEPTION 'O campo "duracao em meses" não pode estar vazio';
+    end if;
+    if (tg_op = 'update') THEN
+    	new.atualizado_em := CURRENT_TIMESTAMP;
+        new.atualizado_por := CURRENT_USER;
+    end if;
+    return new;
+
+ end ;
+ $$ language plpgsql; 
+ 
+ create or replace trigger valida_treino
+ before insert or UPDATE on treino
+ for each ROW
+ execute procedure func_valida_treino();
+
+
