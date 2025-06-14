c2be3971f8dce2393671305a2683cb3911549935
GiseleBCSantos <giselebianca57@gmail.com>
2025-06-14 11:41:02 -0300

    Refactor SQL table creation scripts to include 'deleted' column and improve structure

--- criacao-tabelas.sql
+++ criacao-tabelas.sql
@@ -1,143 +1,152 @@
 -- Metodo de pagamento
-create table  metodo_pagamento
-(
-  id_met_pag serial primary KEY,
-  nome varchar(50) not null,
-  descricao varchar(200) not null,
+DROP TABLE IF EXISTS metodo_pagamento CASCADE;
+CREATE TABLE metodo_pagamento (
+  id_met_pag SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  descricao VARCHAR(200) NOT NULL,
+  taxa_percentual NUMERIC(5,2) NOT NULL DEFAULT 0.00,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  atualizado_por varchar(50) null,
-  atualizado_em TIMESTAMP NULL
- );
- 
- -- Plano
- create table plano
- (
-  id_pla serial primary key,
-  nome varchar(50) not null,
-  descricao varchar(200) not null,
-  duracao_meses int not null,
-  valor NUMERIC(10,2) not null,
-  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
-  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   atualizado_por varchar(50) null,
-  atualizado_em TIMESTAMP NULL
- );
- 
- -- Aluno
- create table aluno
- (
-   id_aluno serial primary key,
-   nome varchar(100) not null,
-   telefone varchar(50),
-   email varchar(50),
-   dt_nasc date not null,
-   genero CHAR NOT NULL CHECK (genero IN ('M', 'F', 'O')),
-   dt_cadastro date not null,
-   endereco varchar(100),
-   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
-   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   atualizado_por varchar(50) null,
-   atualizado_em TIMESTAMP NULL
- );
- 
- -- funcionarios
- create table funcionario
- (
-   id_func serial primary key,
-   nome varchar(100) not null,
-   telefone varchar(50),
-   email varchar(50),
-   endereco varchar(100), 
-   dt_nasc date not null,
-   dt_admissao date not null,
-   turno varchar(20) not null,
-   carga_horaria int not null,
-   salario NUMERIC(10,2) not null,
-   tipo char not null,
-   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
-   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   atualizado_por varchar(50) null,
-   atualizado_em TIMESTAMP NULL
- );
- 
- create table instrutor
- (
-   qntd_alunos int,
-   especialidade varchar(20)
- ) INHERITS  (funcionario);
- 
- create table atendente
- (
-  permissao_caixa char
- )
- INHERITS  (funcionario);
- 
- -- equipamento
- create table equipamento
- (
-   id_equip serial primary key,
-   nome varchar(50) not null,
-   descricao varchar(200) not null,
-   data_aq date not null,
-   idade_min int not null,
-   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
-   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   atualizado_por varchar(50) null,
-   atualizado_em TIMESTAMP NULL
- );
- 
- -- treino
- create table treino
- (
-   id_trei serial primary key,
-   descricao varchar(200) not null,
-   objetivo varchar(50),
-   duracao_meses int,
-   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
-   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   atualizado_por varchar(50) null,
-   atualizado_em TIMESTAMP NULL
- ); 
- 
- -- item treino
- create table item_treino 
- (
-  id_item_treino serial primary key,
-  series int not null,
-  repeticoes int not null,
-  carga int,
-  id_trei int not null,
-  id_equip int not null,
-  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
-  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-   atualizado_por varchar(50) null,
-  atualizado_em TIMESTAMP NULL,
-  foreign key (id_trei) references treino(id_trei),
-  foreign key (id_equip) references equipamento(id_equip)
- );
- 
--- treino aluno
-create table treino_aluno
-(
-  id_trei_alu serial primary key,
-  dt_inicio date not null,
-  observacao varchar(200),
-  id_func int not null,
-  id_aluno int not null,
-  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
-  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  atualizado_por varchar(50) null,
-  atualizado_em TIMESTAMP NULL,
-  foreign key (id_func) references funcionario (id_func),
-  foreign key (id_aluno) references aluno (id_aluno)
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP
 );
 
+-- Plano
+DROP TABLE IF EXISTS plano CASCADE;
+CREATE TABLE plano (
+  id_pla SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  descricao VARCHAR(200) NOT NULL,
+  duracao_meses INT NOT NULL,
+  valor NUMERIC(10,2) NOT NULL,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP
+);
 
+-- Aluno
+DROP TABLE IF EXISTS aluno CASCADE;
+CREATE TABLE aluno (
+  id_aluno SERIAL PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  telefone VARCHAR(50),
+  email VARCHAR(50),
+  dt_nasc DATE NOT NULL,
+  genero CHAR NOT NULL CHECK (genero IN ('M', 'F', 'O')),
+  dt_cadastro DATE NOT NULL,
+  endereco VARCHAR(100),
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP
+);
 
+-- Funcionário
+DROP TABLE IF EXISTS funcionario CASCADE;
+CREATE TABLE funcionario (
+  id_func SERIAL PRIMARY KEY,
+  nome VARCHAR(100) NOT NULL,
+  telefone VARCHAR(50),
+  email VARCHAR(50),
+  endereco VARCHAR(100),
+  dt_nasc DATE NOT NULL,
+  dt_admissao DATE NOT NULL,
+  turno VARCHAR(20) NOT NULL,
+  carga_horaria INT NOT NULL,
+  salario NUMERIC(10,2) NOT NULL,
+  tipo CHAR NOT NULL,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP
+);
 
+-- Instrutor
+DROP TABLE IF EXISTS instrutor CASCADE;
+CREATE TABLE instrutor (
+  qntd_alunos INT,
+  especialidade VARCHAR(20),
+  deleted BOOLEAN NOT NULL DEFAULT FALSE
+) INHERITS (funcionario);
 
--- matricula
+-- Atendente
+DROP TABLE IF EXISTS atendente CASCADE;
+CREATE TABLE atendente (
+  permissao_caixa CHAR,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE
+) INHERITS (funcionario);
+
+-- Equipamento
+DROP TABLE IF EXISTS equipamento CASCADE;
+CREATE TABLE equipamento (
+  id_equip SERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  descricao VARCHAR(200) NOT NULL,
+  data_aq DATE NOT NULL,
+  idade_min INT NOT NULL,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP
+);
+
+-- Treino
+DROP TABLE IF EXISTS treino CASCADE;
+CREATE TABLE treino (
+  id_trei SERIAL PRIMARY KEY,
+  descricao VARCHAR(200) NOT NULL,
+  objetivo VARCHAR(50),
+  duracao_meses INT,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP
+);
+
+-- Item Treino
+DROP TABLE IF EXISTS item_treino CASCADE;
+CREATE TABLE item_treino (
+  id_item_treino SERIAL PRIMARY KEY,
+  series INT NOT NULL,
+  repeticoes INT NOT NULL,
+  carga INT,
+  id_trei INT NOT NULL,
+  id_equip INT NOT NULL,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP,
+  FOREIGN KEY (id_trei) REFERENCES treino(id_trei),
+  FOREIGN KEY (id_equip) REFERENCES equipamento(id_equip)
+);
+
+-- Treino Aluno
+DROP TABLE IF EXISTS treino_aluno CASCADE;
+CREATE TABLE treino_aluno (
+  id_trei_alu SERIAL PRIMARY KEY,
+  dt_inicio DATE NOT NULL,
+  observacao VARCHAR(200),
+  id_func INT NOT NULL,
+  id_aluno INT NOT NULL,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
+  criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  atualizado_por VARCHAR(50),
+  atualizado_em TIMESTAMP,
+  FOREIGN KEY (id_func) REFERENCES funcionario(id_func),
+  FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno)
+);
+
+-- Matrícula
+DROP TABLE IF EXISTS matricula CASCADE;
 CREATE TABLE matricula (
   id_matricula SERIAL PRIMARY KEY,
   id_aluno INT NOT NULL,
@@ -147,15 +156,13 @@ CREATE TABLE matricula (
   status VARCHAR(20) CHECK (status IN ('ativo', 'inativo', 'cancelado')),
   valor_pag NUMERIC(10,2),
   dt_matricula DATE NOT NULL,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
   criado_por VARCHAR(50) NOT NULL DEFAULT current_user,
   criado_em TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   atualizado_por VARCHAR(50),
   atualizado_em TIMESTAMP,
-
   FOREIGN KEY (id_aluno) REFERENCES aluno(id_aluno),
   FOREIGN KEY (id_plano) REFERENCES plano(id_pla),
   FOREIGN KEY (id_met_pag) REFERENCES metodo_pagamento(id_met_pag),
   FOREIGN KEY (id_atendente) REFERENCES funcionario(id_func)
 );
-
-
