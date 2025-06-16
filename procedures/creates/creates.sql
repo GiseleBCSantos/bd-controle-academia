141602ead9c4f80f648e068b05dd0dc5f061b16a
kovokar <pkovokar@gmail.com>
2025-06-15 21:28:00 -0300

    procedure inserir  dinamico

---
+++ procedures/creates/creates.sql
@@ -0,0 +1,15 @@
+-- Criação de uma procedure para inserir dados dinamicamente em uma tabela
+CREATE OR REPLACE PROCEDURE inserir_dinamico(
+    p_tabela TEXT,
+    p_colunas TEXT,
+    p_valores TEXT
+    )
+    LANGUAGE plpgsql
+    AS $$
+    DECLARE
+        sql_comando TEXT;
+    BEGIN
+        sql_comando := format('INSERT INTO %I (%s) VALUES (%s)', p_tabela, p_colunas, p_valores);
+        EXECUTE sql_comando;
+    END;
+    $$;
